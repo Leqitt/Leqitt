b4f835f2dbfec911fe95cea726f31de49d7732f9
Leqitt <leqitfire@gmail.com>
2023-05-24 16:30:30 +1000

    Update 2
    Added Javascript to allow a button to work.

--- Assignment/code.js
+++ Assignment/code.js
@@ -1,42 +1,122 @@
-// Ball properties
-var ball = {
-  x: 200,
-  y: 0,
-  radius: 20,
-  color: "red",
-  velocity: 0,
-};
+// Set up canvas
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-// Canvas properties
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-var gravityInput = document.getElementById("gravity");
-
-// Update ball position based on gravity and bouncing
-function update() {
-  // Clear the canvas
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  // Update ball position
-  ball.y += ball.velocity;
-  ball.velocity += parseFloat(gravityInput.value) / 100;
-
-  // Check for bouncing
-  if (ball.y + ball.radius >= canvas.height) {
-    ball.y = canvas.height - ball.radius;
-    ball.velocity *= -0.8; // Reduce the velocity on bouncing
+// Particle class
+class Particle {
+  constructor(x, y, size, color, speedX, speedY) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.color = color;
+    this.speedX = speedX;
+    this.speedY = speedY;
   }
 
-  // Draw the ball
-  ctx.beginPath();
-  ctx.arc(ball.x, ball.y, ball.radius, 0, 2 * Math.PI);
-  ctx.fillStyle = ball.color;
-  ctx.fill();
-  ctx.closePath();
+  draw() {
+    ctx.fillStyle = this.color;
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.closePath();
+    ctx.fill();
+  }
 
-  // Request animation frame
-  requestAnimationFrame(update);
+  update() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+
+    if (this.x + this.size > canvas.width || this.x - this.size < 0) {
+      this.speedX = -this.speedX;
+    }
+
+    if (this.y + this.size > canvas.height || this.y - this.size < 0) {
+      this.speedY = -this.speedY;
+    }
+
+    this.draw();
+  }
 }
 
-// Start the simulation
-update();
+// Temperature control
+let temperature = 50; // Initial temperature
+
+const temperatureRange = document.getElementById('temperatureRange');
+const temperatureValue = document.getElementById('temperatureValue');
+
+temperatureRange.addEventListener('input', () => {
+  temperature = temperatureRange.value;
+  temperatureValue.textContent = temperature;
+});
+
+// State of Matter control
+let state = 'solid'; // Initial state of matter
+
+const stateSelect = document.getElementById('stateSelect');
+
+stateSelect.addEventListener('change', () => {
+  state = stateSelect.value;
+  createParticles();
+});
+
+// Create particles
+let particles = [];
+let numParticles = 100;
+const particleSize = 5;
+const colors = ['#FF0000', '#00FF00', '#0000FF'];
+
+function createParticles() {
+  particles = [];
+
+  const particleSpeed = temperature / 10;
+
+  if (state === 'solid') {
+    for (let i = 0; i < numParticles; i++) {
+      const x = Math.random() * canvas.width;
+      const y = Math.random() * canvas.height;
+      const size = particleSize;
+      const color = colors[Math.floor(Math.random() * colors.length)];
+      const speedX = 0;
+      const speedY = 0;
+
+      particles.push(new Particle(x, y, size, color, speedX, speedY));
+    }
+  } else if (state === 'liquid') {
+    for (let i = 0; i < numParticles; i++) {
+      const x = Math.random() * canvas.width;
+      const y = Math.random() * canvas.height;
+      const size = particleSize;
+      const color = colors[Math.floor(Math.random() * colors.length)];
+      const speedX = (Math.random() - 0.5) * particleSpeed;
+      const speedY = (Math.random() - 0.5) * particleSpeed;
+
+      particles.push(new Particle(x, y, size, color, speedX, speedY));
+    }
+  } else if (state === 'gas') {
+    for (let i = 0; i < numParticles; i++) {
+      const x = canvas.width / 2;
+      const y = canvas.height / 2;
+      const size = particleSize;
+      const color = colors[Math.floor(Math.random() * colors.length)];
+      const angle = Math.random() * Math.PI * 2;
+      const speedX = Math.cos(angle) * particleSpeed;
+      const speedY = Math.sin(angle) * particleSpeed;
+
+      particles.push(new Particle(x, y, size, color, speedX, speedY));
+    }
+  }
+}
+
+// Animation loop
+function animate() {
+  requestAnimationFrame(animate);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  particles.forEach((particle) => {
+    particle.update();
+  });
+}
+
+createParticles();
+animate();
